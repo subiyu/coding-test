ff9017952ea49eab8299a1159948b4cfa3233c5f
subiyu <151658441+subiyu@users.noreply.github.com>
2024-03-20 16:51:12 +0900

    Add files via upload

---
+++ baekjoon-online-judge/search/back_tracking/15649.py
@@ -0,0 +1,17 @@
+def back_tracking(n, m, result, visited):
+    if len(result) == m:
+        print(' '.join(map(str, result)))
+        return
+
+    for i in range(1, n+1):
+        if not visited[i]:
+            result.append(i)
+            visited[i] = True
+            back_tracking(n, m, result, visited)
+            result.pop()
+            visited[i] = False
+
+N, M = map(int, input().split(" "))
+visited = [False] * (N+1)
+result = []
+back_tracking(N, M, result, visited)
